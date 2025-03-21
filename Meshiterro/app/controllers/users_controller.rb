974f6ba065cf418b09b9463366256d6bd2601902
EC2 Default User <ec2-user@ip-172-31-39-141.ap-northeast-1.compute.internal>
2025-03-21 07:06:47 +0000

    [Finish]アプリケーションを作成しよう2-16

--- Meshiterro/app/controllers/users_controller.rb
+++ Meshiterro/app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def update
+    @user = current_user.id
+    @user.update
+    redirect_to show_path
+  end 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end
